e6d8654ba16f903f51afcf05afa13b62312ebd02
martianlantern <martianlantern@gmail.com>
2026-02-28 06:24:36 +0000

    updating the bash script

--- run_codex.sh
+++ run_codex.sh
@@ -4,4 +4,5 @@ LOGFILE="codex_logs_$(date +'%d_%b_%Y_%H_%M_%S')_${UUID}.log"
 mkdir -p agent_logs
 codex exec --model gpt-5.2 \
     --yolo --sandbox danger-full-access \
-    "Hello summarize this project" &> "agent_logs/$LOGFILE"
+    --model-reasoning-effort "high" \
+    "$(cat PROMPT.md)" &> "agent_logs/$LOGFILE"
